651aa8bdde5421f48627d897c8c430d38a7dc7eb
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-01-25 16:57:49 +0100

    aggiunta possibilità di vedere i valori inseriti

--- js/script.js
+++ js/script.js
@@ -10,6 +10,9 @@ let name ;
 // comando usato per interfaccia con utente ed associazione a variabile scritta in blu
 name = prompt ("quale è il tuo nome?")
 
+// comando che ci permette tramite l'inspector di vedere il valore salvato nella variabile
+console.log(name);
+
 
 // dichiarazione variabile
 let cognome;
@@ -18,11 +21,19 @@ let cognome;
 // comando usato per interfaccia con utente ed associazione a variabile scritta in blu
 cognome = prompt ("quale è il tuo cognome?")
 
+// comando che ci permette tramite l'inspector di vedere il valore salvato nella variabile
+console.log(cognome);
+
 // dichiarazione variabile
 let colore;
 
 // comando usato per interfaccia con utente ed associazione a variabile scritta in blu
 colore = prompt ("quale è il tuo colore preferito?")
+
+// comando che ci permette tramite l'inspector di vedere il valore salvato nella variabile
+console.log(colore);
+
+
 
 // coamndo usato per dichiarazione a schermo 
 document.getElementById("title").innerHTML = `Questa è la tua password poco sicura ${name}${cognome}${colore}${666} !!!!`;
