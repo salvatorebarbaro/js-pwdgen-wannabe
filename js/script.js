d62794c41d682afc5f6823583536b9b098f4acdd
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-01-25 16:54:04 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
-document.getElementById("title").innerHTML = 
-"Pagina di salvatore barbaro"
-;
+
+
+// inizio esercizio di oggi
+
+
+// dichiarazione variabile
+let name ;
+
+
+// comando usato per interfaccia con utente ed associazione a variabile scritta in blu
+name = prompt ("quale è il tuo nome?")
+
+
+// dichiarazione variabile
+let cognome;
+
+
+// comando usato per interfaccia con utente ed associazione a variabile scritta in blu
+cognome = prompt ("quale è il tuo cognome?")
+
+// dichiarazione variabile
+let colore;
+
+// comando usato per interfaccia con utente ed associazione a variabile scritta in blu
+colore = prompt ("quale è il tuo colore preferito?")
+
+// coamndo usato per dichiarazione a schermo 
+document.getElementById("title").innerHTML = `Questa è la tua password poco sicura ${name}${cognome}${colore}${666} !!!!`;
+
+// fine
